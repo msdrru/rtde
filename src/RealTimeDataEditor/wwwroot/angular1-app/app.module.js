6004e9bf82d240bb91b6cca88cd1a86984800675
Yatajga <yatajga@hotmail.com>
2016-09-21 20:11:39 +0400

    New RealTimeDataEditor.AspNetCore project added. Angular code refactored to use "Angular 1 Style Guide". Directory for Angular 2 added.

---
+++ src/RealTimeDataEditor/wwwroot/angular1-app/app.module.js
@@ -0,0 +1,14 @@
+﻿(function() {
+    'use strict';
+    angular.module('application', []);
+
+    // SignalR's hub object.
+    var productMessageHub = $.connection.productMessageHub;
+
+    $(function() {
+        $.connection.hub.logging = true;
+        $.connection.hub.start();
+    });
+
+    angular.module('application').value('productMessageHub', productMessageHub);
+})();
---
+++ src/RealTimeDataEditor/wwwroot/angular1-app/products/products.service.js
@@ -0,0 +1,15 @@
+﻿(function() {
+    'use strict';
+    angular.module('application')
+        .factory('productsService',
+            function($http, $q) {
+                return {
+                    get: function() {
+                        var deferred = $q.defer();
+                        $http.get('/api/Products').success(deferred.resolve).error(deferred.reject);
+                        return deferred.promise;
+                    }
+                }
+            }
+        );
+})();

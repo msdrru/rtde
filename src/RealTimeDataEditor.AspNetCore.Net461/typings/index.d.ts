f27d9d1696036ef7ca4b5ad0692fec1ce1506892
Yatajga <yatajga@hotmail.com>
2017-05-04 18:48:31 +0400

    RealTimeDataEditor.AspNetCore.Net461 was modified to use Angular 4.

--- src/RealTimeDataEditor.AspNetCore.Net461/typings/index.d.ts
+++ src/RealTimeDataEditor.AspNetCore.Net461/typings/index.d.ts
@@ -1,4 +1,2 @@
 ﻿/// <reference path="jquery/jquery.d.ts" />
 /// <reference path="signalr/signalr.d.ts" />
-/// <reference path="../node_modules/@types/node/index.d.ts" />
-/// <reference path="../node_modules/typescript/lib/lib.es6.d.ts" />

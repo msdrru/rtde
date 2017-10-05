ea5193f42b049a4d1a80139cf0156a4a928c3de1
Karen Tazayan <yatajga@outlook.com>
2017-10-05 13:09:17 +0400

    RealTimeDataEditor.AspNetCore.Net461 was renamed to Rtde.AspNetCore1.1.Net4.6.1 and recreated.

---
+++ src/Rtde.AspNetCore1.1.Net4.6.1/wwwroot/angular2-app/app/products/products-signalr.service.ts
@@ -0,0 +1,50 @@
+﻿import { Inject, Injectable, EventEmitter } from '@angular/core';
+
+declare var $: any;
+
+@Injectable()
+export class ProductsSignalRService {
+
+    private productMessageHub: any;
+    private connection: any;
+
+    public productChanged: EventEmitter<any>;
+    public messageReceived: EventEmitter<any>;
+    public connectionEstablished: EventEmitter<Boolean>;
+    public connectionExists: Boolean;
+
+    constructor( @Inject('BASE_URL') private baseUrl: string) {
+        this.productChanged = new EventEmitter();
+        this.connectionEstablished = new EventEmitter<Boolean>();
+        this.messageReceived = new EventEmitter<any>();
+        this.connectionExists = false;
+
+        this.connection = $.hubConnection(baseUrl + 'signalr/');
+        this.productMessageHub = this.connection.createHubProxy('productMessageHub');
+
+        this.registerOnServerEvents();
+
+        this.startConnection();
+    }
+
+    public sendChatMessage(message: any) {
+        this.productMessageHub.invoke('SendMessage', message);
+    }
+
+    private startConnection(): void {
+        this.connection.start().done((data: any) => {
+            console.log('Now connected ' + data.transport.name + ', connection ID= ' + data.id);
+            this.connectionEstablished.emit(true);
+            this.connectionExists = true;
+        }).fail((error: any) => {
+            console.log('Could not connect ' + error);
+            this.connectionEstablished.emit(false);
+        });
+    }
+
+    private registerOnServerEvents(): void {
+        this.productMessageHub.on('HandleProductMessage', (data: any) => {
+            this.productChanged.emit(data);
+        });
+    }
+}
---
+++ src/Rtde.AspNetCore1.1.Net4.6.1/wwwroot/angular2-app/app/products/products.component.ts
@@ -0,0 +1,58 @@
+﻿import { Component, OnInit } from '@angular/core'
+import { NgForm } from '@angular/forms';
+import { ProductsService } from './products.service'
+import { ProductsSignalRService } from './products-signalr.service'
+import { Product } from './product'
+
+@Component({
+    moduleId: module.id,
+    selector: 'products',
+    templateUrl: 'products.component.html'
+})
+export class ProductsComponent implements OnInit {
+    public canSendMessage: Boolean;
+
+    selectedRow: Number;
+    selectedProduct : Product;
+
+    errorMessage: string;
+
+    public products: Product[];
+    mode = 'Observable';
+
+    constructor(private productsService: ProductsService,
+        private productsSignalRService: ProductsSignalRService) {
+    }
+
+    public ngOnInit() {
+        this.subscribeToEvents();
+        this.canSendMessage = this.productsSignalRService.connectionExists;
+        this.getProducts();
+    }
+
+    public addNewProduct(): void {
+        console.log("addNewProduct");
+    }
+
+    deleteProduct() {
+        console.log("addNewProduct");
+    }
+
+    private getProducts() {
+        this.productsService.getProducts()
+            .subscribe(
+                (products: any) => this.products = products,
+                error => this.errorMessage = <any>error);
+    }
+
+    private subscribeToEvents(): void {
+        this.productsSignalRService.connectionEstablished.subscribe(() => {
+            this.canSendMessage = true;
+        });
+    }
+
+    public setClickedRow(/*index: any*/): void {
+        //this.selectedRow = index;
+        //this.selectedProduct = product;
+    }
+}
